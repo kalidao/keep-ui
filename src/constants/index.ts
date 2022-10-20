039c5eadaeeb19451b8c66f93f7c769ef6ba2ba8
Shiv <shivanshityagi@icloud.com>
2022-10-20 14:09:54 +0900

    🍬 Add ABIs

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,6 @@
-import KEEP_FACTORY from './abis/KeepFactory.json'
+import { KEEP_FACTORY_ABI } from './abis/KeepFactory'
+import { KEEP_ABI } from './abis/Keep'
 
-const KEEP_FACTORY_ADDRESS = '0x843D08081A10b408bAe2cf21b5dB61F1cF297028'
+const KEEP_FACTORY_ADDRESS = '0x752ac8fC6d8daa00Cf13b344b3fCE338423cdE47'
 
-export { KEEP_FACTORY, KEEP_FACTORY_ADDRESS }
+export { KEEP_FACTORY_ABI, KEEP_ABI, KEEP_FACTORY_ADDRESS }
