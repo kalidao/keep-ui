4a435b43e65ff00f450263ff77f526ef6c0abb26
Shiv <shivanshityagi@icloud.com>
2022-10-26 22:04:08 +0530

    update footer

---
+++ design/Link.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import { link } from './Link.css'
+import type { LinkProps as NextLinkProps } from 'next/link'
+
+type LinkProps = {
+  children: React.ReactNode
+}
+
+export default function CustomLink({ href, children, ...props }: LinkProps & NextLinkProps) {
+  return (
+    <Link href={href} {...props}>
+      <a className={link}>{children}</a>
+    </Link>
+  )
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -43,7 +43,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <RainbowKitProvider chains={chains} theme={darkTheme()}>
         <ThemeProvider defaultMode="dark">
           <QueryClientProvider client={queryClient}>
-          <Component {...pageProps} />
+            <Component {...pageProps} />
           </QueryClientProvider>
         </ThemeProvider>
       </RainbowKitProvider>
