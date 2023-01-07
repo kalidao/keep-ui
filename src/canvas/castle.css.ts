48d4c9db916ca9bce70c5b37f48a7578bf07c689
Shiv <shivanshityagi@icloud.com>
2023-01-07 22:46:15 +0530

    Castle darkmode sketch

--- src/canvas/castle.css.ts
+++ src/canvas/castle.css.ts
@@ -3,10 +3,10 @@ import { style } from '@vanilla-extract/css'
 export const path = style({
   //  style="fill:#ffffff;fill-opacity:0;fill-rule:nonzero;stroke:#000000;stroke-width:3;stroke-dasharray:none;stroke-opacity:1"
   // convert css
-  fill: '#ffffff',
+
   fillOpacity: 0,
   fillRule: 'nonzero',
-  stroke: '#000000',
+
   strokeWidth: 3,
   strokeDasharray: 'none',
   strokeOpacity: 1,
