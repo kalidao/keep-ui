83d2cfe6e9793732589216b7bc4f1a7895a20750
Shiv <shivanshityagi@icloud.com>
2023-01-07 22:32:34 +0530

    upgrade

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,7 @@ import Balencer from 'react-wrap-balancer'
 import { ConnectButton } from '~/components/ConnectButton'
 import { Menu } from '@design/Menu'
 import { Castle } from '~/canvas/Castle'
+import { Slide } from 'react-awesome-reveal'
 
 const Home: NextPage = () => {
   const {} = useQuery(['allKeeps'], () => fetcher(`${process.env.NEXT_PUBLIC_KEEP_API}/keeps/all`))
@@ -59,33 +60,32 @@ const Home: NextPage = () => {
       <Box className={container}>
         <Box className={styles.hero}>
           <Box>
-            <Box display="flex" flexDirection={'column'}>
-              <Balencer>
-                <Box as="h1" className={styles.heading}>
-                  Keep.
-                </Box>
-              </Balencer>
-              {/* marketing copy for no-code legally complian multisig + dao launcher */}
-              <Balencer>
-                <Box as="h2" className={styles.subheading}>
-                  Secure your digital assets.
-                </Box>
-                <Box as="h2" className={styles.subheading}>
-                  Grow a community.
-                </Box>
-              </Balencer>
-            </Box>
-            <Stack direction={'horizontal'}>
-              <Link href="/create" legacyBehavior>
-                <Button as="a" variant="secondary">
-                  Get Started
-                </Button>
-              </Link>
-              <Link href="/explore" legacyBehavior>
-                <Button as="a" variant="transparent">
-                  Explore
-                </Button>
-              </Link>
+            <Stack>
+              <Slide>
+                <Balencer>
+                  <Box as="h1" className={styles.heading}>
+                    Keep.
+                  </Box>
+                </Balencer>
+                {/* marketing copy for no-code legally complian multisig + dao launcher */}
+                <Balencer>
+                  <Box as="h2" className={styles.subheading}>
+                    Secure assets for community.
+                  </Box>
+                </Balencer>
+                <Stack direction={'horizontal'}>
+                  <Link href="/create" legacyBehavior>
+                    <Button as="a" variant="secondary">
+                      Get Started
+                    </Button>
+                  </Link>
+                  <Link href="/explore" legacyBehavior>
+                    <Button as="a" variant="transparent">
+                      Explore
+                    </Button>
+                  </Link>
+                </Stack>
+              </Slide>
             </Stack>
           </Box>
           <Castle />
@@ -95,21 +95,25 @@ const Home: NextPage = () => {
         {features.map((feature, i) => {
           return (
             <Box key={i} className={i % 2 == 0 ? styles.feature : styles.featureReverse}>
-              <Stack align="flex-start">
-                <Box className={styles.featureTitle}>{feature.title}</Box>
-                <Box className={styles.featureDescription}>{feature.description}</Box>
-                <Box as="a" href={feature.link} className={styles.featureStyled}>
-                  {feature.styledFeature}
-                </Box>
-              </Stack>
+              <Slide>
+                <Stack align="flex-start">
+                  <Box className={styles.featureTitle}>{feature.title}</Box>
+                  <Box className={styles.featureDescription}>{feature.description}</Box>
+                  <Box as="a" href={feature.link} className={styles.featureStyled}>
+                    {feature.styledFeature}
+                  </Box>
+                </Stack>
+              </Slide>
               {feature.image && (
-                <Image
-                  src={feature.image}
-                  alt={`${feature.title} image`}
-                  width={800}
-                  height={500}
-                  className={styles.featureImage}
-                />
+                <Slide direction="right">
+                  <Image
+                    src={feature.image}
+                    alt={`${feature.title} image`}
+                    width={800}
+                    height={500}
+                    className={styles.featureImage}
+                  />
+                </Slide>
               )}
             </Box>
           )
