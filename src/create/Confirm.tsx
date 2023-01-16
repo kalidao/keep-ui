540715983dd772f64b994407bf58f8bb08037727
Shiv <shivanshityagi@icloud.com>
2023-01-16 21:53:02 +0530

    fix call

--- src/create/Confirm.tsx
+++ src/create/Confirm.tsx
@@ -120,7 +120,7 @@ export const Confirm = () => {
                 website: state.website,
               },
             }
-            fetch('/api/keeps', {
+            fetch(`${process.env.NEXT_PUBLIC_KEEP_API}/keeps/setup`, {
               method: 'POST',
               headers: {
                 'Content-Type': 'application/json',
