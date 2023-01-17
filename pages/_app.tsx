f72a526df3e6e1b40fe6bb40b2835c035c65a904
Shiv <shivanshityagi@icloud.com>
2023-01-17 23:50:20 +0530

    max

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -47,10 +47,7 @@ function MyApp({ Component, pageProps, theme }: AppProps & { theme: Mode }) {
         >
           <DynamicWagmiConnector>
             <Head>
-              <meta
-                name="viewport"
-                content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
-              />
+              <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0" />
               <link rel="icon" href="/favicon.ico" />
             </Head>
             <main className={inter.className}>
