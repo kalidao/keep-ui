a5734ae84a4d1c5eeba5aa77007a3d9c8f6cd300
Shiv <shivanshityagi@icloud.com>
2023-01-18 00:02:20 +0530

    pwa

--- next.config.js
+++ next.config.js
@@ -3,13 +3,17 @@ const {
 } = require('@vanilla-extract/next-plugin');
 const withVanillaExtract = createVanillaExtractPlugin();
 
+const withPWA = require('next-pwa')({
+  dest: 'public'
+})
+
 const nextConfig = {
+  productionBrowserSourceMaps: true,
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ['gateway.pinata.cloud', 'api.kali.gg'],
   },
- 
 }
 
-module.exports = withVanillaExtract(nextConfig)
+module.exports = withVanillaExtract(withPWA(nextConfig))
