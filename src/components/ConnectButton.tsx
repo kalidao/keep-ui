a98a9d3a3ca99a942f61f5031bb876d70aef89f0
Shiv <shivanshityagi@icloud.com>
2023-01-30 17:40:20 +0800

    Revert "update"
    This reverts commit 3059bc4286e767eb1994deccfd16e693c6a965ff.

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -1,39 +1,13 @@
 import { DynamicWidget } from '@dynamic-labs/sdk-react'
 import { useThemeStore } from '~/hooks'
-import { useAccount, useConnect, useDisconnect } from 'wagmi'
 
 export const ConnectButton = () => {
-  const { address, connector, isConnected } = useAccount()
-  const { connect, connectors, error, isLoading, pendingConnector } = useConnect()
-  const { disconnect } = useDisconnect()
+  const mode = useThemeStore((state) => state.mode)
 
-  if (isConnected) {
-    return (
-      <div className="main">
-        <div className="title">Connected to {connector?.name}</div>
-        <div>{address}</div>
-        <button className="card" onClick={disconnect as any}>
-          Disconnect
-        </button>
-      </div>
-    )
-  } else {
-    return (
-      <div className="main">
-        {connectors.map((connector) => (
-          <button
-            className="card"
-            disabled={!connector.ready}
-            key={connector.id}
-            onClick={() => connect({ connector })}
-          >
-            {connector.name}
-            {!connector.ready && ' (unsupported)'}
-            {isLoading && connector.id === pendingConnector?.id && ' (connecting)'}
-          </button>
-        ))}
-        {error && <div>{error.message}</div>}
-      </div>
-    )
-  }
+  return (
+    <DynamicWidget
+      buttonClassName={mode == 'dark' ? 'connectButtonDark' : 'connectButtonLight'}
+      innerButtonComponent="Login"
+    />
+  )
 }
