e5b1f443cd0c1fd309aad032ab30ef5dc5885498
ross <92001561+z0r0z@users.noreply.github.com>
2023-05-27 09:39:45 +0530

    🥢

--- src/propose/tx/burn_token/index.tsx
+++ src/propose/tx/burn_token/index.tsx
@@ -49,15 +49,15 @@ export const BurnToken = () => {
       />
       <Divider />
       <Input
-        label={`From Address`}
-        placeholder="0x or ENS"
-        type="text"
-        defaultValue={burn_token.fromAddress}
-        {...register('fromAddress', {
-          validate: (value) => isAddressOrEns(value) || 'Not a valid address or ENS.',
-        })}
-        error={errors?.fromAddress && <>{errors?.fromAddress?.message}</>}
-      />
+  label={`From Address`}
+  placeholder="0x or ENS"
+  type="text"
+  defaultValue={burn_token.fromAddress}
+  {...register('fromAddress', {
+    validate: (value) => (value ? isAddressOrEns(value) : false) || 'Not a valid address or ENS.',
+  })}
+  error={errors?.fromAddress && <>{errors?.fromAddress?.message}</>}
+/>
     </Box>
   )
 }
