563e107adbd3f1e28e20f927030eccdc3d3e9796
ross <92001561+z0r0z@users.noreply.github.com>
2023-06-19 14:23:56 +0530

    🧹  Tidy mint app

--- src/propose/tx/mint_token/index.tsx
+++ src/propose/tx/mint_token/index.tsx
@@ -1,32 +1,33 @@
-import { useEffect } from 'react'
-
-import { Box, Divider, Input } from '@kalidao/reality'
-import { useFormContext } from 'react-hook-form'
-import { useSendStore } from '~/propose/tx/useSendStore'
-import { MintTokenProps } from '~/propose/types'
-import { isAddressOrEns, validateEns } from '~/utils/ens'
+import { useEffect } from 'react';
+import { Box, Divider, Input } from '@kalidao/reality';
+import { useFormContext } from 'react-hook-form';
+import { useSendStore } from '~/propose/tx/useSendStore';
+import { MintTokenProps } from '~/propose/types';
+import { isAddressOrEns } from '~/utils/ens';
 
 export const MintToken = () => {
-  const mint_token = useSendStore((state) => state.mint_token)
-  const setMintToken = useSendStore((state) => state.setMintToken)
+  const mint_token = useSendStore((state) => state.mint_token);
+  const setMintToken = useSendStore((state) => state.setMintToken);
 
   const {
     register,
     formState: { errors },
     watch,
-  } = useFormContext<MintTokenProps>()
+  } = useFormContext<MintTokenProps>();
 
-  const [watchedId, watchedAmount, watchedAddress] = watch(['id', 'amount', 'address'])
+  const [watchedId, watchedAmount, watchedAddress] = watch(['id', 'amount', 'address']);
 
   useEffect(() => {
-    if (watchedId !== undefined && watchedAmount !== undefined && watchedAddress !== undefined) {
+    if (watchedId !== undefined &&
+        watchedAmount !== undefined &&
+        watchedAddress !== undefined) {
       setMintToken({
         id: watchedId,
         amount: watchedAmount,
         address: watchedAddress as `0xstring`,
-      })
+      });
     }
-  }, [watchedId, watchedAmount, watchedAddress, setMintToken])
+  }, [watchedId, watchedAmount, watchedAddress, setMintToken]);
 
   return (
     <Box display={'flex'} flexDirection="column" gap="2">
@@ -35,18 +36,14 @@ export const MintToken = () => {
         placeholder="ID"
         type="number"
         defaultValue={mint_token.id}
-        {...register('id', {
-          valueAsNumber: true,
-        })}
+        {...register('id', { valueAsNumber: true })}
       />
       <Input
         label={`Amount`}
         placeholder="Amount"
         type="number"
         defaultValue={mint_token.amount}
-        {...register('amount', {
-          valueAsNumber: true,
-        })}
+        {...register('amount', { valueAsNumber: true })}
       />
       <Divider />
       <Input
@@ -55,10 +52,12 @@ export const MintToken = () => {
         type="text"
         defaultValue={mint_token.address}
         {...register('address', {
-          validate: (value) => (value ? isAddressOrEns(value) : false) || 'Not a valid address or ENS.',
+          validate: (value) =>
+            (value ? isAddressOrEns(value) : false) ||
+            'Not a valid address or ENS.',
         })}
         error={errors?.address && <>{errors?.address?.message}</>}
       />
     </Box>
-  )
-}
+  );
+};
