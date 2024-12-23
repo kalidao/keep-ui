f2dd20849b59672b9c44cbb3d658cb62bb6f15c9
Shivanshi Tyagi <shivanshityagi@icloud.com>
2024-12-23 18:00:21 +0700

    add unstable injected connector

--- components/root-provider.tsx
+++ components/root-provider.tsx
@@ -8,8 +8,9 @@ import { siteConfig } from '@/lib/site-config'
 import { RainbowKitProvider, getDefaultConfig } from '@rainbow-me/rainbowkit'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from 'sonner'
-import { WagmiProvider } from 'wagmi'
+import { WagmiProvider, fallback, http, unstable_connector } from 'wagmi'
 import { arbitrum, base, mainnet, optimism, polygon } from 'wagmi/chains'
+import { injected } from 'wagmi/connectors'
 
 if (!process.env.NEXT_PUBLIC_WC_ID) {
   throw new Error('NEXT_PUBLIC_WC_ID is required')
@@ -22,6 +23,13 @@ const config = getDefaultConfig({
   projectId: process.env.NEXT_PUBLIC_WC_ID,
   chains: [mainnet, polygon, optimism, arbitrum, base],
   ssr: true, // If your dApp uses server side rendering (SSR)
+  transports: {
+    [mainnet.id]: fallback([unstable_connector(injected), http()]),
+    [polygon.id]: fallback([unstable_connector(injected), http()]),
+    [optimism.id]: fallback([unstable_connector(injected), http()]),
+    [arbitrum.id]: fallback([unstable_connector(injected), http()]),
+    [base.id]: fallback([unstable_connector(injected), http()]),
+  },
 })
 
 const RootProvider: FC<PropsWithChildren<{}>> = ({ children }) => {
